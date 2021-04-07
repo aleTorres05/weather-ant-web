54250dd7c6650b4f787eff0ce34c9c5c28cea408
aleTorres05 <torresalexatlanta@gmail.com>
2021-04-07 16:23:03 -0500

    Weather web app with ant

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/lib/layout/layout";
+
+function App() {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+}
+export default App;
